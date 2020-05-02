82861749f9ac0630354480e5c5ada0510a3a817d
Jake Weinstein <jake@aospa.co>
2020-05-03 00:52:07 +0200

    SnapdragonCamera: Rename package to "co.p404.camera".
    Change-Id: I7e62fd9ff5b0f9b6449e5cf143826e9733de5b69

--- Android.mk
+++ Android.mk
@@ -25,10 +25,12 @@ LOCAL_AAPT_FLAGS := \
         --version-name "$(version_name_package)" \
         --version-code $(version_code_package) \
 
-LOCAL_PACKAGE_NAME := SnapdragonCamera
+LOCAL_PACKAGE_NAME := P404Camera
 LOCAL_VENDOR_MODULE := true
 LOCAL_PRIVILEGED_MODULE := true
 #LOCAL_PRIVATE_PLATFORM_APIS:=true
+
+LOCAL_AAPT_FLAGS += --rename-manifest-package co.p404.camera
 
 LOCAL_SDK_VERSION := current
 LOCAL_RENDERSCRIPT_TARGET_API := 23
